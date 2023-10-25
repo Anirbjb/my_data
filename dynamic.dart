c3c6a3e0be5634d278a3250b960992066d10b3b1
Anir <146140599+Anirbjb@users.noreply.github.com>
2023-10-25 21:00:54 +0700

    Create dynamic.dart

---
+++ dynamic.dart
@@ -0,0 +1,21 @@
+class MyData<T>{
+  T data;
+  MyData(this.data);
+
+}
+  void printData(MyData data)
+  print(dataString) MyData<String>("Anir");
+  dataNumber = MyData(100);
+  dataBool = MyData(true);
+
+
+
+
+void main(){
+  
+  
+ 
+  printData(MyData("Anir"));
+  printData(MyData(100));
+  printData(MyData(true));
+}
